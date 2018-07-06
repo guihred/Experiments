cac0cf8d2161bddd15012cc9d87eccf759ab07ca
guilherme.hmedeiros <guih.red@gmail.com>
2018-07-06 04:29:52 -0300

    Creation of spliting file routine

--- src/main/java/ml/CSVUtils.java
+++ src/main/java/ml/CSVUtils.java
@@ -1,10 +1,22 @@
 package ml;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CSVUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CSVUtils.class);
     private static final char DEFAULT_SEPARATOR = ',';
     private static final char DEFAULT_QUOTE = '"';
 
@@ -42,7 +54,6 @@ public class CSVUtils {
                     inQuotes = false;
                     doubleQuotesInColumn = false;
                 } else {
-
                     // Fixed : allow "" in custom quote enclosed
                     if (ch == '\"') {
                         if (!doubleQuotesInColumn) {
@@ -52,30 +63,22 @@ public class CSVUtils {
                     } else {
                         curVal.append(ch);
                     }
-
                 }
             } else {
                 if (ch == customQuote) {
-
                     inQuotes = true;
-
                     // Fixed : allow "" in empty quote enclosed
                     if (chars[0] != '"' && customQuote == '\"') {
                         curVal.append('"');
                     }
-
                     // double quotes in column will hit this!
                     if (startCollectChar) {
                         curVal.append('"');
                     }
-
                 } else if (ch == separators) {
-
-                    result.add(curVal.toString());
-
+                    result.add(curVal.toString().replaceFirst("" + DEFAULT_QUOTE, ""));
                     curVal = new StringBuilder();
                     startCollectChar = false;
-
                 } else if (ch == '\r') {
                     continue;
                 } else if (ch == '\n') {
@@ -84,12 +87,52 @@ public class CSVUtils {
                     curVal.append(ch);
                 }
             }
-
         }
-
         result.add(curVal.toString());
-
         return result;
     }
+
+    public static void splitFile(String csvFile, int columnIndex) {
+        Map<String, Writer> hashMap = new HashMap<>();
+        File file = new File("out");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+
+	    try (Scanner scanner = new Scanner(new File(csvFile));) {
+            String firstLine = scanner.nextLine();
+            while (scanner.hasNext()) {
+                String nextLine = scanner.nextLine();
+                List<String> parseLine = CSVUtils.parseLine(nextLine);
+                String string = parseLine.get(columnIndex);
+                if (!hashMap.containsKey(string)) {
+                    Writer output = createWriter("out/" + csvFile.replaceAll("\\..+", "") + string + ".csv");
+                    output.append(firstLine + "\n");
+                    hashMap.put(string, output);
+                }
+                hashMap.get(string).append(nextLine + "\n");
+            }
+	    }catch(Exception e) {
+            LOGGER.error("ERROR ", e);
+	    }
+    }
+
+    private static Writer createWriter(String csvFile) throws IOException {
+        try {
+            File file = new File(csvFile);
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+            return new BufferedWriter(new FileWriter(csvFile, true));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        splitFile("WDIData.csv", 3);
+    }
+	
 
 }
