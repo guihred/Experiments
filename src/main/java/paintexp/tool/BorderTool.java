df183be2ee5b969e56da4c45c71674b422dabe68
guigu <guih.red@gmail.com>
2018-11-29 13:45:08 -0200

    Border tool with rectangle

--- src/main/java/paintexp/tool/BorderTool.java
+++ src/main/java/paintexp/tool/BorderTool.java
@@ -32,7 +32,7 @@ public class BorderTool extends RectangleTool {
 	    model.getToolOptions().getChildren().clear();
         model.getToolOptions().setSpacing(5);
         model.getToolOptions().getChildren()
-                .add(new SimpleSliderBuilder(1, 50, 10).bindBidirectional(length).prefWidth(50).build());
+				.add(new SimpleSliderBuilder(1, 7, 2).bindBidirectional(length).prefWidth(50).build());
 	
 	}
 
@@ -40,10 +40,17 @@ public class BorderTool extends RectangleTool {
     protected void drawBorders(final int layoutX, final int layoutY,final int width, final int height, final PaintModel model) {
 		Color[][] color2 = new Color[width][height];
 		PixelHelper pixel = new PixelHelper();
+		PixelHelper pixel2 = new PixelHelper();
+		PixelHelper pixel3 = new PixelHelper();
+		PixelHelper pixel4 = new PixelHelper();
 		PixelReader reader = model.getImage().getPixelReader();
+		int round = 1 << length.get();
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				pixel.reset();
+				pixel2.reset();
+				pixel3.reset();
+				pixel4.reset();
 				if (withinRange(i + layoutX, j + layoutY, model)) {
 					for (int k = -1; k < 2; k++) {
 						for (int l = -1; l < 2; l++) {
@@ -51,9 +58,16 @@ public class BorderTool extends RectangleTool {
 							int y = setWithinRange(j + l + layoutY, 0, height - 1 + layoutY);
 							int argb2 = reader.getArgb(x, y);
 							pixel.add(argb2, k + l);
+							pixel2.add(argb2, k - l);
+							pixel3.add(argb2, -k + l);
+							pixel4.add(argb2, -k - l);
 						}
 					}
-					color2[i][j] = pixel.toColor(2).invert();
+					int argb = pixel.toArgb(round);
+					int argb2 = pixel2.toArgb(round);
+					int argb3 = pixel3.toArgb(round);
+					int argb4 = pixel4.toArgb(round);
+					color2[i][j] = PixelHelper.asColor(argb | argb2 | argb3 | argb4).invert();
 				}
 			}
 		}
--- src/main/java/paintexp/tool/PixelHelper.java
+++ src/main/java/paintexp/tool/PixelHelper.java
@@ -31,6 +31,14 @@ class PixelHelper {
 		a = b = r = g = 0;
 	}
 
+	public int toArgb(final int round) {
+		int red = PaintTool.setWithinRange(i == 0 ? r : r / i, 0, 255) / round * round;
+		int green = PaintTool.setWithinRange(i == 0 ? g : g / i, 0, 255) / round * round;
+		int blue = PaintTool.setWithinRange(i == 0 ? b : b / i, 0, 255) / round * round;
+		int transp = PaintTool.setWithinRange(i == 0 ? 255 : a / i, 0, 255);
+		return transp << 24 | red << 16 | green << 8 | blue;
+	}
+
 	public Color toColor() {
 		int red = PaintTool.setWithinRange(i == 0 ? r : r / i, 0, 255);
 		int green = PaintTool.setWithinRange(i == 0 ? g : g / i, 0, 255);
@@ -47,4 +55,11 @@ class PixelHelper {
 		return Color.rgb(red, green, blue, transp);
 	}
 
+	public static Color asColor(final int argb) {
+		int a = argb >> 24 & 0xFF;
+		int r = argb >> 16 & 0xFF;
+		int g = argb >> 8 & 0xFF;
+		int b = argb & 0xFF;
+		return Color.rgb(r, g, b, a / (double) 255);
+	}
 }
