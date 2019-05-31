691c603243ecd46fb570049b1dda14635876e813
guilherme.hmedeiros <guilherme.hmedeiros@BSAD275627.GOV>
2019-05-31 08:39:28 -0300

    adding new properties in processScan

--- src/main/java/ethical/hacker/ProcessScan.java
+++ src/main/java/ethical/hacker/ProcessScan.java
@@ -9,12 +9,23 @@ import utils.HasLogging;
 
 public class ProcessScan {
 
-	private static final Logger LOG = HasLogging.log();
+    private static final Logger LOG = HasLogging.log();
+
+    public static void main(String[] args) {
+        scanProcesses();
+    }
+
+    public static List<Map<String, String>> scanCurrentTasks() {
+        List<String> processes = ConsoleUtils
+            .executeInConsoleInfo("wmic process get Name,ProcessId,ParentProcessId,SessionId /FORMAT:csv");
+//        Node,Name,ParentProcessId,ProcessId,SessionId
+        List<String> title = new ArrayList<>();
+        return processes.stream().map(e -> e.trim().split(",")).map(e -> Stream.of(e).collect(Collectors.toList()))
+            .map(key -> createMap(title, key)).filter(e -> !e.isEmpty()).collect(Collectors.toList());
+    }
 
     public static List<String> scanProcesses() {
-
-        List<String> executionInfo = ConsoleUtils.executeInConsoleInfo(
-                " netstat -aon");
+        List<String> executionInfo = ConsoleUtils.executeInConsoleInfo(" netstat -aon");
         List<String[]> ports = new ArrayList<>();
         for (String string : executionInfo) {
             if (string.matches("  [^P].+")) {
@@ -24,30 +35,18 @@ public class ProcessScan {
                 ports.add(fields);
             }
         }
-        
         return executionInfo;
-
     }
 
-    public static List<Map<String, String>> scanCurrentTasks() {
-        List<String> processes = ConsoleUtils.executeInConsoleInfo(
-                " tasklist /NH /FO CSV");
-//        "Nome da imagem","Identificação pessoal","Nome da sessão","Sessão#","Uso de memória"
-        return processes.stream().map(e -> e.trim().split("^|\",*\"*"))
-                .map(e -> Stream.of(e).map(s -> s.replaceAll("\"", "")).collect(Collectors.toList()))
-                .map(key -> {
-                    Map<String, String> hashMap = new LinkedHashMap<>();
-                    hashMap.put("Nome da imagem", key.get(0));
-                    hashMap.put("Identificação pessoal", key.get(1));
-                    hashMap.put("Nome da sessão", key.get(2));
-                    hashMap.put("Sessão", key.get(3));
-                    hashMap.put("Uso de memória", key.get(4));
-                    return hashMap;
-                }).collect(Collectors.toList());
-    }
-
-
-    public static void main(String[] args) {
-        scanProcesses();
+    private static Map<String, String> createMap(List<String> title, List<String> key) {
+        Map<String, String> hashMap = new LinkedHashMap<>();
+        if (title.isEmpty() && key.size() > 1) {
+            title.addAll(key);
+        } else if (key.size() > 1) {
+            for (int i = 0; i < title.size(); i++) {
+                hashMap.put(title.get(i), key.get(i));
+            }
+        }
+        return hashMap;
     }
 }
