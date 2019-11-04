be514a4cab97fabce2c9eddd3042ac5db744fca1
guilherme.hmedeiros <guilherme.hmedeiros@BSAD275627.GOV>
2019-11-04 08:32:26 -0300

    Creating pie graph

--- src/main/java/extract/FileAttrApp.java
+++ src/main/java/extract/FileAttrApp.java
@@ -2,32 +2,32 @@
 package extract;
 
 import java.io.File;
-import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.SplitPane;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import simplebuilder.SimpleTableViewBuilder;
 import simplebuilder.SimpleTreeViewBuilder;
-import utils.*;
+import utils.ConsumerEx;
+import utils.CrawlerTask;
+import utils.StageHelper;
+import utils.SupplierEx;
 
 public class FileAttrApp extends Application {
-    private SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
     private String[] sizes = { "B", "KB", "MB", "GB", "TB" };
     private Map<File, BasicFileAttributes> attrMap = new LinkedHashMap<>();
     private Map<File, Long> sizeMap = new LinkedHashMap<>();
+    private ObservableList<Data> pieData = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage primaryStage) {
@@ -56,39 +56,28 @@ public class FileAttrApp extends Application {
 
     private Parent createSplitTreeListDemoNode() {
         File rootFile = new File("").getAbsoluteFile();
-        ObservableList<BasicFileAttributes> files = FXCollections.observableArrayList(getAttributes(rootFile));
         SimpleTreeViewBuilder<File> root = new SimpleTreeViewBuilder<File>().root(rootFile).cellFactory(this::setText)
-            .onSelect(ConsumerEx.makeConsumer(t -> onSelectFile(files, t)));
-        SimpleTableViewBuilder<BasicFileAttributes> builder = new SimpleTableViewBuilder<BasicFileAttributes>()
-            .items(files);
-        List<Method> allMethodsRecursive = ClassReflectionUtils.getAllMethodsRecursive(BasicFileAttributes.class);
-        allMethodsRecursive.sort(Comparator.comparing(Method::getName));
+            .onSelect(ConsumerEx.makeConsumer(this::onSelectFile));
 
-        Map<Class<?>, FunctionEx<Object, ?>> formatterMap = new HashMap<>();
-        formatterMap.put(long.class, i -> getFileSize((Long) i));
-        formatterMap.put(FileTime.class, i -> getDate((FileTime) i));
-
-        for (Method method : allMethodsRecursive) {
-            FunctionEx<Object, ?> functionEx = formatterMap.getOrDefault(method.getReturnType(), i -> i);
-            builder.addColumn(method.getName(), (ob, cell) -> cell
-                .setText(Objects.toString(SupplierEx.get(() -> functionEx.apply(method.invoke(ob))))));
-        }
-        TableView<BasicFileAttributes> tableView = builder.equalColumns().minWidth(200).build();
-        SplitPane splitPane = new SplitPane(root.build(), tableView);
+        PieChart pieChart = new PieChart();
+        SplitPane splitPane = new SplitPane(root.build(), pieChart);
         splitPane.setDividerPositions(1. / 5);
+        pieChart.setData(pieData);
+        pieChart.setTitle("Directory Files");
+        pieChart.setLegendVisible(false);
+        pieChart.setClockwise(false);
+        pieChart.setStartAngle(90);
+        pieChart.setLabelsVisible(true);
         return new VBox(StageHelper.selectDirectory("Select Directory", "Select Directory", f -> {
             root.build().getRoot().getChildren().clear();
             root.root(f);
-            files.set(0, getAttributes(f));
-        }), splitPane);
+        }), splitPane, pieChart
+
+        );
     }
 
     private BasicFileAttributes getAttributes(File value) {
         return attrMap.computeIfAbsent(value, this::computeAttributes);
-    }
-
-    private String getDate(FileTime date) {
-        return df.format(date.toMillis());
     }
 
     private String getFileSize(long sizeInBytes) {
@@ -98,19 +87,27 @@ public class FileAttrApp extends Application {
     }
 
     private long getSize(File file) {
-        return sizeMap.computeIfAbsent(file, this::computeSize) ;
+        return sizeMap.computeIfAbsent(file, this::computeSize);
     }
 
-    private void onSelectFile(ObservableList<BasicFileAttributes> files, TreeItem<File> t) {
+    private void onSelectFile(TreeItem<File> t) {
         if (t != null) {
             File value = t.getValue();
-            files.set(0, getAttributes(value));
-            if (value.isDirectory() && t.getChildren().isEmpty()) {
+            if (value.isDirectory()) {
                 File[] listFiles = value.listFiles();
                 if (listFiles != null) {
+                    pieData.clear();
+                    Arrays.parallelSort(listFiles, Comparator.comparing(e -> !e.isDirectory()));
                     for (File file2 : listFiles) {
-                        t.getChildren().add(new TreeItem<>(file2));
+                        pieData.add(new PieChart.Data(file2.getName(), getSize(file2)));
                     }
+                    pieData.sort(Comparator.comparing(Data::getPieValue));
+                    if (t.getChildren().isEmpty()) {
+                        for (File file2 : listFiles) {
+                            t.getChildren().add(new TreeItem<>(file2));
+                        }
+                    }
+
                 }
             }
         }
