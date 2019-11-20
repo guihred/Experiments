740e058e28a35de7756ffb98f4f188d8d85dfea5
guilherme.hmedeiros <guilherme.hmedeiros@BSAD275627.GOV>
2019-11-20 06:46:43 -0300

    Extracting complexity

--- src/main/java/gaming/ex23/TicTacToeLauncher.java
+++ src/main/java/gaming/ex23/TicTacToeLauncher.java
@@ -2,11 +2,7 @@ package gaming.ex23;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.BiFunction;
-import java.util.function.IntFunction;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -17,12 +13,10 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import simplebuilder.SimpleDialogBuilder;
 import utils.CommonsFX;
 
 public class TicTacToeLauncher extends Application {
 
-    private static final int SIZE = 3;
     @FXML
     private GridPane gridPane;
     private int currentPlayer;
@@ -39,7 +33,7 @@ public class TicTacToeLauncher extends Application {
         if (square.getState() == TicTacToePlayer.NONE) {
             square.setState(players.get(currentPlayer++ % players.size()));
         }
-        verifyWin();
+        TicTacToeHelper.verifyWin(squares, gridPane);
     }
 
     @Override
@@ -47,73 +41,8 @@ public class TicTacToeLauncher extends Application {
         CommonsFX.loadFXML("Tic-Tac-Toe", "TicTacToeLauncher.fxml", this, stage);
     }
 
-    private boolean anyWin(int i) {
-        if (anyWinner(j -> getSquare(j, j).getState())) {
-            return true;
-        }
-        if (anyWinner(j -> getSquare(j, 2 - j).getState())) {
-            return true;
-        }
-        if (anyWinner(j -> getSquare(i, j).getState())) {
-            return true;
-        }
-        return anyWinner(j -> getSquare(j, i).getState());
-    }
-
-    private TicTacToeSquare getSquare(int i, int j) {
-        return squares.get(i * SIZE + j);
-    }
-
-    private TicTacToePlayer getWinner() {
-        List<BiFunction<Integer, Integer, TicTacToePlayer>> a = Arrays.asList((i, j) -> getSquare(j, j).getState(),
-            (i, j) -> getSquare(j, 2 - j).getState(), (i, j) -> getSquare(i, j).getState(),
-            (i, j) -> getSquare(j, j).getState(), (i, j) -> getSquare(j, i).getState());
-        for (BiFunction<Integer, Integer, TicTacToePlayer> intFunction : a) {
-            Optional<TicTacToePlayer> findFirst = IntStream.range(0, SIZE)
-                .mapToObj(i -> getWinner(j -> intFunction.apply(i, j))).filter(Optional<TicTacToePlayer>::isPresent)
-                .map(Optional<TicTacToePlayer>::get).findFirst();
-            if (findFirst.isPresent()) {
-                return findFirst.get();
-            }
-        }
-        return TicTacToePlayer.NONE;
-    }
-
-    private void reset() {
-        for (TicTacToeSquare sq : squares) {
-            sq.setState(TicTacToePlayer.NONE);
-        }
-    }
-
-    private void verifyWin() {
-        if (IntStream.range(0, SIZE).anyMatch(this::anyWin)) {
-            TicTacToePlayer winner = getWinner();
-            new SimpleDialogBuilder().text(winner + " Won!").button("Reset", this::reset).bindWindow(gridPane)
-                .displayDialog();
-            return;
-        }
-        if (IntStream.range(0, SIZE).mapToObj(i -> IntStream.range(0, SIZE).mapToObj(j -> getSquare(i, j).getState()))
-            .flatMap(e -> e).noneMatch(e -> e == TicTacToePlayer.NONE)) {
-            new SimpleDialogBuilder().text("It's a draw!").button("Reset", this::reset).bindWindow(gridPane)
-                .displayDialog();
-
-        }
-    }
-
     public static void main(String[] args) {
         launch(args);
-    }
-
-    private static boolean anyWinner(IntFunction<TicTacToePlayer> mapper) {
-        long count = IntStream.range(0, SIZE).mapToObj(mapper).distinct().count();
-        return count == 1 && IntStream.range(0, SIZE).mapToObj(mapper).noneMatch(e -> e == TicTacToePlayer.NONE);
-    }
-
-    private static Optional<TicTacToePlayer> getWinner(IntFunction<TicTacToePlayer> mapper) {
-        if (anyWinner(mapper)) {
-            return IntStream.range(0, SIZE).mapToObj(mapper).findFirst();
-        }
-        return Optional.empty();
     }
 
 }
