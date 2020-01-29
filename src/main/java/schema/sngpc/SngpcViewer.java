51e57ff89a79102d11ad81184d7a077fb7183e12
guilherme.hmedeiros <guilherme.hmedeiros@BSAD275627.GOV>
2020-01-29 12:06:45 -0300

    Adding exporting capabilities

--- src/main/java/schema/sngpc/SngpcViewer.java
+++ src/main/java/schema/sngpc/SngpcViewer.java
@@ -3,7 +3,9 @@ package schema.sngpc;
 
 import static schema.sngpc.XMLExtractor.newMap;
 import static schema.sngpc.XMLExtractor.readXMLFile;
+import static simplebuilder.SimpleButtonBuilder.newButton;
 
+import extract.ExcelService;
 import java.io.File;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -14,11 +16,13 @@ import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.w3c.dom.Node;
 import simplebuilder.SimpleTableViewBuilder;
 import simplebuilder.SimpleTreeViewBuilder;
+import utils.FunctionEx;
 import utils.ResourceFXUtils;
 import utils.StageHelper;
 
@@ -61,7 +65,34 @@ public class SngpcViewer extends Application {
 
         Button importXMLButton = StageHelper.chooseFile("Import XML", "Import XML",
             newFile -> readXMLFile(tree, allItems, newFile), "Xml", "*.xml");
-        return new VBox(importXMLButton, new SplitPane(tree, sideTable));
+        Button exportExcel = newButton("Export excel", e -> exportToExcel(tree, file));
+        return new VBox(new HBox(importXMLButton, exportExcel), new SplitPane(tree, sideTable));
+    }
+
+    private static void exportToExcel(TreeView<Map<String, String>> tree, File file) {
+        TreeItem<Map<String, String>> selectedItem = tree.getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
+            return;
+        }
+
+        ObservableList<TreeItem<Map<String, String>>> lista = selectedItem.getChildren();
+        Map<String, FunctionEx<TreeItem<Map<String, String>>, Object>> mapa = new HashMap<>();
+        int orElse = lista.stream().mapToInt(i -> i.getChildren().size()).max().orElse(0);
+        for (int j = 0; j < orElse; j++) {
+            int k = j;
+            mapa.put("" + j, t -> getValue(t, k));
+        }
+
+        ExcelService.getExcel(lista, mapa, ResourceFXUtils.getOutFile(file.getName().replaceAll(".xml", ".xlsx")));
+    }
+
+    private static Object getValue(TreeItem<Map<String, String>> i, int j) {
+        ObservableList<TreeItem<Map<String, String>>> children = i.getChildren();
+        if (j < children.size()) {
+            return children.get(j).getValue().values().stream().collect(Collectors.joining("\n"));
+        }
+
+        return FunctionEx.mapIf(i.getValue(), m -> i.getValue().values().toString(), "");
     }
 
     private static void onSelectTreeItem(ObservableList<Map<String, String>> list,
