e639e8d97d394db7e4bd75f4362be331a67c5629
guilherme.hmedeiros <guilherme.hmedeiros@BSAD275627.GOV>
2020-02-21 10:38:38 -0300

    Improving bucket functionality

--- src/main/java/paintexp/tool/BucketTool.java
+++ src/main/java/paintexp/tool/BucketTool.java
@@ -1,95 +1,132 @@
 package paintexp.tool;
+
 import static utils.DrawOnPoint.withinImage;
 
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.EventType;
 import javafx.scene.Node;
+import javafx.scene.control.Slider;
 import javafx.scene.image.PixelReader;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import simplebuilder.SimpleSliderBuilder;
+import utils.DrawOnPoint;
 import utils.PixelHelper;
 
 public class BucketTool extends PaintTool {
 
-	private int width;
-	private int height;
+    private int width;
+    private int height;
 
+    private IntegerProperty threshold = new SimpleIntegerProperty(PixelHelper.MAX_BYTE / 20);
 
-	@Override
-	public Node createIcon() {
+    private Slider thresholdSlider;
+
+    @Override
+    public Node createIcon() {
         return PaintTool.getIconByURL("Bucket.png");
-	}
+    }
 
-	@Override
-	public void handleEvent(final MouseEvent e, final PaintModel model) {
-		EventType<? extends MouseEvent> eventType = e.getEventType();
+    @Override
+    public void handleEvent(final MouseEvent e, final PaintModel model) {
+        EventType<? extends MouseEvent> eventType = e.getEventType();
         if (MouseEvent.MOUSE_CLICKED.equals(eventType)) {
-			onMouseClicked(e, model);
-		}
+            onMouseClicked(e, model);
+        }
         if (MouseEvent.MOUSE_DRAGGED.equals(eventType)) {
             onMouseClicked(e, model);
         }
-	}
+    }
+
+    @Override
+    public void onSelected(final PaintModel model) {
+        model.getToolOptions().getChildren().clear();
+
+        Slider slider = getThresholdSlider();
+        model.getToolOptions().getChildren().add(slider);
+        Text text = new Text();
+        text.textProperty().bind(threshold.divide(slider.getMax()).multiply(100).asString("%.0f%%"));
+        model.getToolOptions().getChildren().add(text);
+    }
 
     public void setColor(final int initX, final int initY, final int originalColor, final int frontColor,
         final PixelReader pixelReader, final PaintModel model) {
-		List<Integer> toGo = new ArrayList<>();
-		toGo.add(index(initX, initY));
-		while (!toGo.isEmpty()) {
-			Integer next = toGo.remove(0);
-			int x = x(next);
-			int y = y(next);
+        List<Integer> toGo = new ArrayList<>();
+        toGo.add(index(initX, initY));
+        PixelHelper pixel = new PixelHelper();
+
+        while (!toGo.isEmpty()) {
+            Integer next = toGo.remove(0);
+            int x = x(next);
+            int y = y(next);
             if (withinImage(x, y, model.getImage())) {
-				int color = pixelReader.getArgb(x, y);
-				if (color == originalColor) {
-					if (y != 0 && y != height - 1) {
-						addIfNotIn(toGo, next + 1);
-						addIfNotIn(toGo, next - 1);
-						addIfNotIn(toGo, next + width);
-						addIfNotIn(toGo, next - width);
-					}
-					model.getImage().getPixelWriter().setArgb(x, y, frontColor);
-				}
-			}
+                pixel.reset(originalColor);
+                int color = pixelReader.getArgb(x, y);
+                if (color != frontColor && (color == originalColor || closeColor(pixel, color))) {
+                    if (y != 0 && y != height - 1) {
+                        addIfNotIn(toGo, next + 1);
+                        addIfNotIn(toGo, next - 1);
+                        addIfNotIn(toGo, next + width);
+                        addIfNotIn(toGo, next - width);
+                    }
+                    model.getImage().getPixelWriter().setArgb(x, y, frontColor);
+                }
+            }
 
-		}
+        }
         model.createImageVersion();
-	}
+    }
 
-	private void addIfNotIn(final List<Integer> toGo, final Integer e) {
+    private void addIfNotIn(final List<Integer> toGo, final Integer e) {
         if (!toGo.contains(e) && e < width * height && e >= 0) {
-			toGo.add(e);
-		}
-	}
+            toGo.add(e);
+        }
+    }
 
-	private Integer index(final int initialX2, final int initialY2) {
+    private boolean closeColor(final PixelHelper pixel, final int color) {
+        pixel.add(color, -1);
+        return pixel.modulus() < threshold.get();
+    }
+
+    private Slider getThresholdSlider() {
+        if (thresholdSlider == null) {
+            thresholdSlider = new SimpleSliderBuilder(0, PixelHelper.MAX_BYTE, 0).bindBidirectional(threshold)
+                .maxWidth(60).build();
+
+        }
+        return thresholdSlider;
+    }
+
+    private Integer index(final int initialX2, final int initialY2) {
         return initialX2 + initialY2 * width;
-	}
+    }
 
-	private void onMouseClicked(final MouseEvent e, final PaintModel model) {
-		int initialX = (int) e.getX();
-		int initialY = (int) e.getY();
-		width = (int) model.getImage().getWidth();
-		height = (int) model.getImage().getHeight();
-		PixelReader pixelReader = model.getImage().getPixelReader();
-		int originalColor = pixelReader.getArgb(initialX, initialY);
+    private void onMouseClicked(final MouseEvent e, final PaintModel model) {
+        int initialX = (int) DrawOnPoint.getWithinRange(e.getX(), 0, model.getImage().getWidth() - 1);
+        int initialY = (int) DrawOnPoint.getWithinRange(e.getY(), 0, model.getImage().getHeight() - 1);
+        width = (int) model.getImage().getWidth();
+        height = (int) model.getImage().getHeight();
+        PixelReader pixelReader = model.getImage().getPixelReader();
+        int originalColor = pixelReader.getArgb(initialX, initialY);
         int frontColor = PixelHelper
             .toArgb(e.getButton() == MouseButton.PRIMARY ? model.getFrontColor() : model.getBackColor());
-		if (originalColor != frontColor) {
-			Platform.runLater(() -> setColor(initialX, initialY, originalColor, frontColor, pixelReader, model));
-		}
-	}
+        if (originalColor != frontColor) {
+            Platform.runLater(() -> setColor(initialX, initialY, originalColor, frontColor, pixelReader, model));
+        }
+    }
 
-	private int x(final int m) {
+    private int x(final int m) {
         return m % width;
-	}
+    }
 
-	private int y(final int m) {
+    private int y(final int m) {
 
         return m / width;
-	}
-
+    }
 
 }
